795cbf7fdc62b5b7c935a2708a6f1a6eea5af51e
bxnt <efgg.efg13@mail.ru>
2026-04-23 19:17:35 +0300

    Initialize Spring Boot Project

---
+++ src/main/java/org/example/ordermanagementapi/OrderManagementApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.ordermanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderManagementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ordermanagementapi/OrderManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ordermanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
